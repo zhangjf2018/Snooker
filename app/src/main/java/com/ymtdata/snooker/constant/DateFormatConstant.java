111eac665431896ec852e11fe33f50a1dff26a02
ABNER <414512194@qq.com>
2018-04-04 18:52:49 +0800

    视图绑定

---
+++ app/src/main/java/com/ymtdata/snooker/constant/DateFormatConstant.java
@@ -0,0 +1,12 @@
+package com.ymtdata.snooker.constant;
+
+/**
+ * Created by zhangjifeng on 2018/4/2.
+ * email : 414512194@qq.com
+ * desc  :
+ */
+
+public class DateFormatConstant {
+    public final static String YYYY_MM_DD = "yyyy-MM-dd";
+    public final static String HH_MM = "HH:mm";
+}
